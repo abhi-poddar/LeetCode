a74db4a47a0b5e2a588fde301d31f056274ce18e
abhi-poddar <156682951+abhi-poddar@users.noreply.github.com>
2024-07-06 16:17:06 +0530

    Time: 5 ms (67.46%) | Memory: 58.1 MB (23.23%) - LeetSync

---
+++ 11-container-with-most-water/container-with-most-water.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int maxArea(int[] height) {
+        int maxVol = Integer.MIN_VALUE;
+        int l = 0;
+        int r = height.length - 1;
+
+        while(l <= r){
+            int length = r - l;
+            int breadth = Math.min(height[l], height[r]);
+            maxVol = Math.max(maxVol, length * breadth);
+            if(height[l] < height[r]){
+                l++;
+            }else{
+                r--;
+            }
+        }
+
+        return maxVol;
+    }
+}
