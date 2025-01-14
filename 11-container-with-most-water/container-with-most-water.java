e60b8699574a7fcaa7fc5f389de7af5f43b82af5
abhi-poddar <156682951+abhi-poddar@users.noreply.github.com>
2025-01-14 19:48:45 +0530

    Time: 5 ms (73.93%) | Memory: 58 MB (53.01%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.java
+++ 11-container-with-most-water/container-with-most-water.java
@@ -4,7 +4,7 @@ class Solution {
         int l = 0;
         int r = height.length - 1;
 
-        while(l <= r){
+        while(l < r){
             int length = r - l;
             int breadth = Math.min(height[l], height[r]);
             maxVol = Math.max(maxVol, length * breadth);
