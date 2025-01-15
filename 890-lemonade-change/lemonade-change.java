593b686b7293843f11b54e093ad152f3d15ff212
abhi-poddar <156682951+abhi-poddar@users.noreply.github.com>
2025-01-15 22:42:26 +0530

    Time: 2 ms (94.64%) | Memory: 56 MB (53.58%) - LeetSync

---
+++ 890-lemonade-change/lemonade-change.java
@@ -0,0 +1,61 @@
+class Solution {
+    public boolean lemonadeChange(int[] bills) {
+        int five = 0;
+        int ten = 0;
+
+        for (int i = 0; i <= bills.length - 1; i++) {
+            if (bills[i] == 5) {
+                five++;
+            } else if (bills[i] == 10) {
+                if (five >= 1) {
+                    five--;
+                    ten++;
+                } else {
+                    return false;
+                }
+            } else if (bills[i] == 20) {
+                if (ten >= 1 && five >= 1) {
+                    ten--;
+                    five--;
+                } else if (five >= 3) {
+                    five -= 3;
+                } else {
+                    return false;
+                }
+            }
+        }
+        return true;
+        // int five=0;
+        // int ten =0;
+        // for(int i=0 ; i < bills.length;i++){
+        //     if(bills[i]==5){
+        //         five++;
+        //     }
+        //     else if(bills[i]==10){
+        //         ten++;
+        //     }
+        // }
+
+        // for(int i=0;i<bills.length;i++){
+        //     if(bills[i]==10 && five>=1){
+        //         return true;
+        //         five=five-1;
+        //     }
+        //     else if(bills[i]==20){
+        //         if(five>=3){
+        //             return true;
+        //             five=five-3;
+        //         }
+        //         else if(ten>=1 && five>=1)
+        //             return true;
+        //             ten=ten-1;
+        //             five=five-1;
+        //         }
+        //         else{
+        //             return false;
+        //         }
+        //     }
+        // }
+        // return true;
+    }
+}
