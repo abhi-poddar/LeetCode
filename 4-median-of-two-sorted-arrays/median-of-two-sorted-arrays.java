9392416518dd6291221fdccaf90c0957cb46975f
abhi-poddar <156682951+abhi-poddar@users.noreply.github.com>
2025-02-13 10:19:31 +0530

    Time: 5 ms (27.37%) | Memory: 46.5 MB (11.78%) - LeetSync

---
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -0,0 +1,23 @@
+class Solution {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int n1 = nums1.length;
+        int n2 = nums2.length;
+        int halfLength = (n1+n2)/2;
+        int[] arr = new int[n1+n2];
+        double result =0;
+        double sum=0;
+        int i=0, j=0, k=0;
+        while(i<nums1.length){
+            arr[k++]=nums1[i++];
+        }
+        while(j<nums2.length){
+            arr[k++]=nums2[j++];
+        }
+        Arrays.sort(arr);
+        int mid = (n1+n2)/2;
+        if((n1+n2)%2==0){
+            return (double)(arr[mid] + arr[mid-1])/2;
+        }
+       return (double)arr[mid];
+    }
+}
