75eea806cd960723a7a039dccfc9bb878b806740
abhi-poddar <156682951+abhi-poddar@users.noreply.github.com>
2025-09-17 09:14:46 +0530

    Time: 7 ms (56.31%) | Memory: 45.7 MB (46.04%) - LeetSync

---
+++ 3462-vowels-game-in-a-string/vowels-game-in-a-string.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean doesAliceWin(String s) {
+        int count =0;
+        for(int i = 0;i<s.length();i++){
+            char val = s.charAt(i);
+            if(val =='a'||val=='e'||val=='i'||val=='o'||val=='u'){
+                count++;
+            }
+
+        }
+        if(count == 0){
+            return false;
+        }
+        if(count%2!=0){
+            return true;
+        }
+        if(count%2==0){
+            return true;
+        }
+        return false;
+    }
+}
